dbf95dc45384b7d897f724a1a9695bc560fe201c
Tasneem Jahan <areenfar@gmail.com>
2022-10-05 21:41:40 +0600

    Completed mid lab task 1

---
+++ Controllers/FormController.cs
@@ -0,0 +1,35 @@
+﻿using Mid_Assignment_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mid_Assignment_2.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("FormSubmitted", "Home");
+            }
+
+            return View(form);
+        }
+    }
+}
---
+++ Models/Form.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mid_Assignment_2.Models
+{
+    public class Form
+    {
+        [Required, StringLength(50, MinimumLength = 3), RegularExpression("^[A-Za-z][A-Za-z_. ,]{2,49}$", ErrorMessage = "Please enter a valid name!")]
+        public string Name { get; set; }
+
+        [Required, RegularExpression("[0-9]{2}-[0-9]{5}-[1-3]{1}", ErrorMessage = "Your ID format is incorrect!")]
+        public string ID { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required, ValidateEmail]
+        public string Email { get; set; }
+
+        [Required, RegularExpression(".(?=.[A-Z])(?=.[a-z])(?=.[0-9])(?=.[!@#$ %^&~><.,:;]).*", ErrorMessage = "Password format is incorrect!")]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        public string Confirm_Password { get; set; }
+
+        [Required, ValidateBirthdate, DataType(DataType.Date)]
+        public DateTime Date_Of_Birth { get; set; }
+            
+    }
+}
